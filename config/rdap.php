18074c77dab3d5b38efb502ab1dca61a4ee770ff
Freek Van der Herten <freek@spatie.be>
2022-05-25 23:47:22 +0200

    wip

--- config/rdap.php
+++ config/rdap.php
@@ -6,13 +6,11 @@ return [
     /*
      * When making an RDAP query, we first have got to make a request to determine
      *  the server responsible for the tld of the query. Here you can specify
-     * how long we should cache the server URLs
+     * how long we should cache the server URLs.
      */
 
     'tld_servers_cache' => [
         'store_name' => null,
         'duration_in_seconds' => CarbonInterval::week()->totalSeconds,
     ],
-
-
 ];
